53aebf7b5abba08f359977a3436cfa3645901eb5
FleterEwenn <fleter.ewenn@gmail.com>
2026-06-16 19:55:43 +0200

    correction des erreurs dans la génération du GPX

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from overpass import get_path
 from dijkstra import dijkstra
 from graphe_manage import create_graphe, create_loop
+from generate_GPX import generate_GPX
 import time
 
 list_path = False
@@ -19,10 +20,11 @@ with open("dijkstra.txt", "w") as f:
 with open("graphe.txt", "w") as f:
     f.write(str(graphe))
 
-loop_path = create_loop(start, shortly_distance, graphe, 100)
+loop_path = create_loop(start, shortly_distance, graphe, 1000)
 
 print(loop_path[1])
 
 with open("path.txt", "w") as f:
     f.write(str(loop_path[0]))
 
+generate_GPX(loop_path[0])
